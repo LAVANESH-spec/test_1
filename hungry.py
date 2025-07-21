854280f4fd2eea3101a04b786ef6539beeb7bc8e
LAVANESH-spec <lavaneshmahendran475@gmail.com>
2025-07-21 11:31:41 -0400

    burger

--- hungry.py
+++ hungry.py
@@ -3,9 +3,5 @@ if hungry=="yes":
     print("Eat samosa");
     print("Eat pizza");
     print("Eat burger");
-    print("Eat fries");
 else:
-    thirsty=input("Are you thirsty:")
-    if thirsty=="yes":
-        print("Drink more Water:");
-        print("Drink soda");
+    print("Do homework")
